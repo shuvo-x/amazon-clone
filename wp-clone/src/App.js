ae991542bd178eca81adc050ef3782c474d80bed
Your Name <you@example.com>
2021-04-25 16:54:01 +0600

    init

---
+++ wp-clone/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+import Product from './Product';
+import {BrowserRouter as Router , Switch , Route}
+from "react-router-dom";
+import Checkout from "./Checkout";
+
+
+function App() {
+  return (
+    <Router >
+    <div className="App">
+    <Header /> 
+   <Switch>
+
+      <Route path="/checkout">
+       
+       <Checkout />
+      </Route>
+       <Route path="/">  
+        
+         <Home />
+         
+        </Route>
+
+   </Switch>      
+    </div>
+    </Router>
+  );
+}
+
+export default App;
